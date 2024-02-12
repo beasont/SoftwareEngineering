7ef32978906c8316c3fbae0b3f2f4a63cd16e30e
beasont <146970907+beasont@users.noreply.github.com>
2024-02-12 15:57:41 +0100

    Create man.py

---
+++ February 12th, 2024/man.py
@@ -0,0 +1,16 @@
+#this is very basic practice
+Man = "John"
+HisAge = 52
+HisHeightCM = 172
+HisHeightFT = "5'8"
+print("There is a man.")
+print("His name is", Man)
+print ("He is", HisAge, "years old.")
+print ("His height in centimeters is...", HisHeightCM)
+print ("His height in feet is...", HisHeightFT)
+print()
+print ("He has the following dogs:")
+dog1, dog2, dog3 = "Golden Retriever,", "German Shepherd,", "and a Siberian Husky."
+print (dog1, dog2, dog3)
+name1, name2, name3 = "Sandy,", "Kraut,", "and Tito."
+print ("And their names, in order, are", name1, name2, name3)
