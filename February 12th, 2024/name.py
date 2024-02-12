5f0ebe4dd93be8be67c791e0200cc0d064c31e8b
beasont <146970907+beasont@users.noreply.github.com>
2024-02-12 16:13:37 +0100

    Create name.py

---
+++ February 12th, 2024/name.py
@@ -0,0 +1,13 @@
+# first assignment:
+
+# The first iteration:
+#FullName = input("What is your full name?\n")
+#print(FullName)
+
+# The final product:
+FullName = input("What is your full name?\n")
+print("Hello " + FullName + ".")
+response = input("How are you doing today?\n")
+print("Excellent.")
+location = input("And where are you from?\n")
+print("Well,", FullName + ", " + "I've heard that", location, "is a great place to live!")
